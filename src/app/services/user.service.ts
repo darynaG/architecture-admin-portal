bf4a1f79fff9c2254e211caf5145be1fde57855b
Dana Hatsosh <dasha.gacosh@gmail.com>
2019-11-08 11:25:02 +0200

    change to service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { User } from '../models/user';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {ConfigService} from '../../config.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +9,8 @@ import {Observable} from 'rxjs';
 export class UserService {
   constructor(private http: HttpClient) { }
 
-  getThresholdsForExtract(extractId: number): Observable<any> {
-    const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${extractId}/thresholds`;
-
-    return this.http.get<PriceThresholdManualCollection>(url, {
-      withCredentials: true
-    });
-  }
-
   getAll(sortField, sortDirection, pageIndex, pageSize): Observable<any> {
-    let url = ConfigService.getApiUrl() + `/v3/price_extracts_manual?pageIndex=${pageIndex}`;
+    const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual?pageIndex=${pageIndex}`;
 
     return this.http.get<any>(url, {
       withCredentials: true
@@ -29,36 +21,40 @@ export class UserService {
     const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${id}`;
 
     return this.http.delete<any>(url, {
-      withCredentials: true,
-      headers: this.authStateService.createAuthHeaders()
+      withCredentials: true
     });
   }
 
-  deleteThreshold(priceThresholdId: number, priceExtractId: number): Observable<any> {
-    const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${priceExtractId}/thresholds/${priceThresholdId}`;
-
-    return this.http.delete<any>(url, {
-      withCredentials: true,
-      headers: this.authStateService.createAuthHeaders()
-    });
-  }
-
-  createPriceExtract(priceExtract): Observable<any> {
+  create(priceExtract): Observable<any> {
     const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual`;
 
     return this.http.put<any>(url, priceExtract, {
-      withCredentials: true,
-      headers: this.authStateService.createAuthHeaders()
+      withCredentials: true
     });
   }
 
-  updatePriceExtract(id, priceExtract): Observable<any> {
+  update(id, priceExtract): Observable<any> {
     const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${id}`;
 
     return this.http.post<any>(url, priceExtract, {
-      withCredentials: true,
-      headers: this.authStateService.createAuthHeaders()
+      withCredentials: true
     });
   }
+
+ /* deleteThreshold(priceThresholdId: number, priceExtractId: number): Observable<any> {
+    const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${priceExtractId}/thresholds/${priceThresholdId}`;
+
+    return this.http.delete<any>(url, {
+      withCredentials: true
+    });
+  }
+
+  getThresholdsForExtract(extractId: number): Observable<any> {
+    const url = ConfigService.getApiUrl() + `/v3/price_extracts_manual/${extractId}/thresholds`;
+
+    return this.http.get<any>(url, {
+      withCredentials: true
+    });
+  }*/
 
 }
--- src/config.service.ts
+++ src/config.service.ts
@@ -4,37 +4,13 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
+  private static apiUrl = '';
 
   public static getEnvironmentVariable(value) {
-    let environment: string;
-    let data = {};
-
-    environment = window.location.hostname;
-
-    switch (environment) {
-      case 'admin-portal.lite.doublehorn.com':
-        data = {
-          apiUrl: 'https://api.doublehorn.com/product-catalog'
-        };
-        break;
-
-      case 'admin-portal.lite-staging.doublehorn.com':
-        data = {
-          apiUrl: 'https://staging.api.doublehorn.com/product-catalog'
-        };
-        break;
-
-      default:
-        data = {
-          apiUrl: 'https://staging.api.doublehorn.com/product-catalog'
-        };
-    }
-
-    return data[value];
+    return  this.apiUrl;
   }
 
   public static getApiUrl() {
     return this.getEnvironmentVariable('apiUrl');
   }
-
 }
